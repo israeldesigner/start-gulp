101a2cad19473986f5e363731e435a398b59dea4
israel <israeldesignerce@outlook.com>
2022-03-15 13:15:08 -0300

    async axios

--- src/js/function.js
+++ src/js/function.js
@@ -80,71 +80,182 @@ AOS.init({
 
 
 
-(async () => {
-  const response = await axios({
-    url: 'https://dog.ceo/api/breeds/list/all',
-    method: 'get'
-  })
+// (async () => {
+//   const response = await axios({
+//     url: 'https://dog.ceo/api/breeds/list/all',
+//     method: 'get'
+//   })
 
-  console.log(response)
-})()
+//   console.log(response)
+// })()
 
 axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 delete axios.defaults.headers.common["X-Requested-With"];
-axios.get('https://dados.fortaleza.ce.gov.br/api/3/action/group_list', {
-  params: {
-    all_fields: true
-  },
-    headers: {                  
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Authorization", 
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
-      "Content-Type": "application/json;charset=UTF-8"                   
-  }  
-},
-)
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-})
-.then(function () {
-  // always executed
-}); 
+
+axios.interceptors.request.use(function(config) {
+  // Do something before request is sent
+  console.log('Start Ajax Call');
+  return config;
+}, function(error) {
+  // Do something with request error
+  console.log('Error');
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function(response) {
+  // Do something with response data
+  let msgLoadGrupos = document.getElementById('msgShow');
+  if(msgLoadGrupos){
+    msgLoadGrupos.style.display = "none";
+  }
+  console.log('Done with Ajax call');
+
+  return response;
+}, function(error) {
+  // Do something with response error
+  console.log('Error fetching the data');
+  return Promise.reject(error);
+});
+
+var resultElement = document.getElementById('getResult1');
+
+const baseUrl = 'https://dados.fortaleza.ce.gov.br/api/3/action/';
+const urlDataSet = 'https://dados.fortaleza.ce.gov.br/dataset/'
+
+const appendToDOM = (users) => {
+  // const ul = document.querySelector('ul');
+  let idDataTable = document.getElementById('dataBody');
+  if(idDataTable){
+    if(users.length > 0 ){
+      var temp = '';
+      users.forEach((itemData, i) => {
+        var resourceFormats = itemData.resources;
+        var tmpResource = '';
+        for (let i = 0; i < resourceFormats.length; i++) {
+          const element = resourceFormats[i];
+          tmpResource += element.format;
+        }
+        if(i < 5){
+          var date = new Date(itemData.metadata_modified);
+          var newDate =  date.toISOString().substring(0, 10);
+          var newDateBr = date.toLocaleString();
+          temp += `<td><a href="/dataset/${itemData.name}">${itemData.title}</a></td>`;
+          temp += "<td>" + newDateBr + "</td>";
+          temp += "<td>" + itemData.organization.title + "</td>";
+          temp += "<td>" + tmpResource + "</td></tr>";
+        }
+      });
+      idDataTable.innerHTML = temp;
+    }
+  }
+};
+
+const appendIconDOM = (icons) => {
+  // const ul = document.querySelector('ul');
+  let iconDataList = document.getElementById('iconListas');
+  if(iconDataList){
+    if(icons.length > 0 ){
+      var temp = '';
+      icons.forEach((icon) => {
+        temp += `
+          <div class='col-lg-2 col-sm-6 col-6 text-center divGrupo-icon'>
+            <a href="/group/${icon.name}">
+              <i class="demo-icon ${icon.name}icon-fol fa-3x icon-circle bg-iconGrad-2 text-white mb-2 "></i>
+              <p>${icon.title}</p>
+              <b>${icon.package_count}</b>
+            </a>
+          </div>
+        `;
+      });
+      iconDataList.innerHTML = temp;
+    }
+  }
+};
 
 
-// document.querySelector('a').addEventListener('click', function (e) {
+const modifiedMetada = () =>{ 
+  axios.get(`${baseUrl}package_search?q=&sort=metadata_modified+desc`, {
+    params: {
+    },
+      headers: {                  
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+        "Content-Type": "application/json;charset=UTF-8"                   
+    }  
+  })
+  .then(function (response) {
+    const resultData = response.data.result.results;
+    appendToDOM(resultData);
+  })
+  .catch(function (error) {
+    console.log(error);
+  }) 
+}
 
-//   // Calling preventDefault will prevent the browser from navigating (prevents the default behavior)
-//   e.preventDefault();
+modifiedMetada()
 
-//   console.log(e);
+async function fetchGroups() {
 
-//   // Our defined link, where we want to navigate to
-//   const destination = this.getAttribute('href');
+  try{
 
-//   // Our loader div - we'll set to a const to reference later
-//   const loader = document.querySelector('#loader-wrapper');
+    const response = await axios.get(`${baseUrl}group_list`,{
+      params: {
+        all_fields: true
+      },
+        headers: {                  
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Authorization", 
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+          "Content-Type": "application/json;charset=UTF-8"                   
+      }      
+    })
+    .then(response => {
+        const data = response.data.result;
+        appendIconDOM(data);
+    })
 
-//   // On click, we immediately want to show our loading div.
-//   // For this demo, we'll use style.display to override our CSS
-//   loader.style.display = 'block';
+  }catch{
+    console.error(error);
+  }
+};
+
+fetchGroups();
+
+
+document.querySelector('a').addEventListener('click', function (e) {
+
+  // Calling preventDefault will prevent the browser from navigating (prevents the default behavior)
+  e.preventDefault();
+
+  console.log(e);
+
+  return
+
+  // Our defined link, where we want to navigate to
+  const destination = this.getAttribute('href');
+
+  // Our loader div - we'll set to a const to reference later
+  const loader = document.querySelector('#loader-wrapper');
+
+  // On click, we immediately want to show our loading div.
+  // For this demo, we'll use style.display to override our CSS
+  loader.style.display = 'block';
   
-//   // Set Timeout - We've defined a 1 second delay
-//   setTimeout(function () {
+  // Set Timeout - We've defined a 1 second delay
+  setTimeout(function () {
 
-//       // After 1 second, we want to hide the loader. Again, using style, we set to 'none'
-//       loader.style.display = 'none';
+      // After 1 second, we want to hide the loader. Again, using style, we set to 'none'
+      loader.style.display = 'none';
 
-//       // One additional set timeout - this allows the browser time to hide the loader. I've defined a half second
-//       setTimeout(function () {
+      // One additional set timeout - this allows the browser time to hide the loader. I've defined a half second
+      setTimeout(function () {
 
-//             // This will set the URL to our href
-//             window.location.href = destination;
-//       }, 500);
-//   }, 1000);
-// }) 
+            // This will set the URL to our href
+            window.location.href = destination;
+      }, 500);
+  }, 1000);
+}) 
 // (async () => {
 //   const response = await axios.get('https://dados.fortaleza.ce.gov.br/api/3/action/group_list?all_fields=true')
 //   console.log(response)
